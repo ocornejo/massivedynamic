180c0dc4e2908fdd62cda57e5149ff7310fd74c8
flansho <franciscojfp@hotmail.com>
2012-05-11 19:42:14 -0400

    login listo, al loguear se crea una sesión y direcciona a Principal, para desloguear entrar a logout

--- application/config/database.php
+++ application/config/database.php
@@ -71,7 +71,21 @@ $db['default']['stricton'] = FALSE;
 
 /*Conexión a catalogo*/
 
-
+$db['default2']['hostname'] = '204.87.169.53';
+$db['default2']['username'] = 'mdynamic';
+$db['default2']['password'] = 'mdynamic_pass';
+$db['default2']['database'] = 'catalogo';
+$db['default2']['dbdriver'] = 'mysql';
+$db['default2']['dbprefix'] = '';
+$db['default2']['pconnect'] = TRUE;
+$db['default2']['db_debug'] = TRUE;
+$db['default2']['cache_on'] = FALSE;
+$db['default2']['cachedir'] = '';
+$db['default2']['char_set'] = 'utf8';
+$db['default2']['dbcollat'] = 'utf8_general_ci';
+$db['default2']['swap_pre'] = '';
+$db['default2']['autoinit'] = TRUE;
+$db['default2']['stricton'] = FALSE;
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
--- application/models/seleccionarusuarios.php
+++ application/models/seleccionarusuarios.php
@@ -2,8 +2,8 @@
 class seleccionarusuarios extends CI_Model{
 function get_usuarios()
 {
-     $this->load->database();
-    $sql = $this->db->query('SELECT * from usuarios');
+     $this->load->database('default2',TRUE);
+    $sql = $this->db->query('SELECT * from Productos');
     return $sql->result();
     /* you simply return the results as an object
      * also note you can use the ActiveRecord class for this...might make it easier
