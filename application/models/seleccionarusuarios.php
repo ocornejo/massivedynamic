d6f60896d66fc02d431d986c9642ad9c03f5487b
flansho <franciscojfp@hotmail.com>
2012-05-12 02:44:35 -0400

    login listo, al loguear se crea una sesión y direcciona a Principal, para desloguear entrar a logout

--- application/models/seleccionarusuarios.php
+++ application/models/seleccionarusuarios.php
@@ -2,17 +2,12 @@
 class seleccionarusuarios extends CI_Model{
 function get_usuarios()
 {
-   $DB2 = $this->load->database('default2', TRUE); 
+   $DB2=$this->load->database("default2", TRUE); 
 
    $query = $DB2->query('SELECT * FROM Productos');
     return $query;
-    /* you simply return the results as an object
-     * also note you can use the ActiveRecord class for this...might make it easier
-     */
+
 }
 }
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 ?>
