16d746ac899d3ade3a1ee34ea52a24655a646ed3
flansho <franciscojfp@hotmail.com>
2012-05-13 00:12:33 -0400

    tabla con los productos y bienvenida

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
+	
 	<title>Welcome to CodeIgniter</title>
 
 	<style type="text/css">
@@ -67,19 +67,10 @@
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Bienvenido a Massive Dynamic!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	<?php 
+			<?php 
         echo "<h2>Los software que hay en la Base de Datos son:</h2></br><table>
     <tr>
         <td>Codigo</td>
