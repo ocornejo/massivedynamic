d913bd7c402aefd99c2fd1e6524530f11eb4c57a
ocornejo <ocornejo@192.168.0.100>
2012-06-08 23:52:57 -0400

    commit

--- application/controllers/pdttest.php
+++ application/controllers/pdttest.php
@@ -7,10 +7,10 @@ class PdtTest extends CI_Controller {
 
     public function ppp() {
         $desc = //set to the order description to be appear on the PayPal website;
-        $orderno = 1;//set to unique order number;
+                $orderno = 1; //set to unique order number;
         $json = file_get_contents('http://currencies.apps.grandtrunk.net/getlatest/usd/clp');
-        $data = (int) json_decode($json, TRUE);//set to productTotal + shipmentFee + tax;
-        $nettotal = (int) (5000/$data);
+        $data = (int) json_decode($json, TRUE); //set to productTotal + shipmentFee + tax;
+        $nettotal = (int) (5000 / $data);
 //Save order information to database using the unique order number with status set as Pending...
 
 
@@ -32,7 +32,7 @@ class PdtTest extends CI_Controller {
                 "<input type='hidden' name='handling' value='0'>\n" .
                 "<input type='hidden' name='cancel_return' value='$cancelURL'>\n" .
                 "<input type='hidden' name='return' value='$returnURL'>\n" .
-                "<input type='image' src='https://www.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif' name='submit' alt='Buy Now' />".
+                "<input type='image' src='https://www.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif' name='submit' alt='Buy Now' />" .
                 "</form>\n" .
                 "<script language='javascript'>document.frmPayPal.submit();'</script>\n'";
 
@@ -40,78 +40,44 @@ class PdtTest extends CI_Controller {
     }
 
     public function index() {
-        echo "HOLA";
-        $ppAcc = "oc77_1338396747_biz@gmail.com";
-        $at = "6dzmGdM2ss-OIeouBGzXLdtdzJfCkpRjdH92pDnxCxSZYHkkG9JDYgtqtGO"; //PDT Identity Token
-        $url = "https://www.sandbox.paypal.com/cgi-bin/webscr"; //Test
-//$url = "https://www.paypal.com/cgi-bin/webscr"; //Live
-        $tx = $_REQUEST["tx"]; //this value is return by PayPal
-        $cmd = "_notify-synch";
-        $post = "tx=$tx&at=$at&amp;cmd=$cmd";
+        $data['cmd'] = "_notify-synch";
+        $data['tx'] = $this->input->get('tx');
+        $data['at'] = "6dzmGdM2ss-OIeouBGzXLdtdzJfCkpRjdH92pDnxCxSZYHkkG9JDYgtqtGO";
 
-//Send request to PayPal server using CURL
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        $result = $this->curl->setUrl("https://www.sandbox.paypal.com/cgi-bin/webscr")->post($data);
+        $deformat = $this->deformat($result);
 
-        $result = curl_exec($ch); //returned result is key-value pair string
-        $error = curl_error($ch);
-
-        if (curl_errno($ch) != 0) //CURL error
-            exit("ERROR: Failed updating order. PayPal PDT service failed.");
-
-        $longstr = str_replace("\r", "", $result);
-        $lines = explode("\n", $longstr);
-        //$lines = split("\n", $longstr);
-
-//parse the result string and store information to array
-        if ($lines[0] == "SUCCESS") {
-//successful payment
-            $ppInfo = array();
-            for ($i = 1; $i < count($lines); $i++) {
-                $parts = split("=", $lines[$i]);
-                if (count($parts) == 2) {
-                    $ppInfo[$parts[0]] = urldecode($parts[1]);
-                }
+        if ($deformat === false) {
+            echo "There was an issue with your request, log data and research further.";
+        } else {
+            if ($deformat['payment_status'] == "Completed") {
+                echo "Your transaction has been completed, and a receipt for your purchase has been emailed to you.<br>You may log into your account at <a href='https://www.paypal.com'>www.paypal.com</a> to view details of this transaction.";
+            } else {
+                echo "Payment might be echeck and still processing as it's not completed. I would suggest showing a thank you page but research this further.";
             }
-
-            $curtime = gmdate("d/m/Y H:i:s");
-//capture the PayPal returned information as order remarks
-            $oremarks =
-                    "##$curtime##\n" .
-                    "PayPal Transaction Information...\n" .
-                    "Txn Id: " . $ppInfo["txn_id"] . "\n" .
-                    "Txn Type: " . $ppInfo["txn_type"] . "\n" .
-                    "Item Number: " . $ppInfo["item_number"] . "\n" .
-                    "Payment Date: " . $ppInfo["payment_date"] . "\n" .
-                    "Payment Type: " . $ppInfo["payment_type"] . "\n" .
-                    "Payment Status: " . $ppInfo["payment_status"] . "\n" .
-                    "Currency: " . $ppInfo["mc_currency"] . "\n" .
-                    "Payment Gross: " . $ppInfo["payment_gross"] . "\n" .
-                    "Payment Fee: " . $ppInfo["payment_fee"] . "\n" .
-                    "Payer Email: " . $ppInfo["payer_email"] . "\n" .
-                    "Payer Id: " . $ppInfo["payer_id"] . "\n" .
-                    "Payer Name: " . $ppInfo["first_name"] . " " . $ppInfo["last_name"] . "\n" .
-                    "Payer Status: " . $ppInfo["payer_status"] . "\n" .
-                    "Country: " . $ppInfo["residence_country"] . "\n" .
-                    "Business: " . $ppInfo["business"] . "\n" .
-                    "Receiver Email: " . $ppInfo["receiver_email"] . "\n" .
-                    "Receiver Id: " . $ppInfo["receiver_id"] . "\n";
-            echo $oremarks;
-
-//Update database using $orderno, set status to Paid
-//Send confirmation email to buyer and notification email to merchant
-//Redirect to thankyou page
         }
 
-//Payment failed
-        else {
-//Delete order information
-//Redirect to failed page
+        echo "<ul>";
+        foreach ($deformat as $key => $value) {
+            echo "<li>" . $key . " ===> " . $value . "</li>";
+        }
+        echo "</ul>";
+    }
+
+    public function deformat($result) {
+        $lines = explode("\n", $result);
+        $keyarray = array();
+
+//Check to see if request was a success
+        if (strcmp($lines[0], "SUCCESS") == 0) {
+            for ($i = 1; $i < count($lines); $i++) {
+                list($key, $val) = explode("=", $lines[$i]);
+                $keyarray[urldecode($key)] = urldecode($val);
+            }
+            return $keyarray;
+        } else {
+//Their was an issue with the request so return false
+            return false;
         }
     }
 
