9101d08a6e85cbb64f16d8e6806e600617c43b78
flansho <franciscojfp@hotmail.com>
2012-06-17 19:50:48 -0400

    facebook

--- application/controllers/controller_pagofacebook.php
+++ application/controllers/controller_pagofacebook.php
@@ -45,7 +45,7 @@ class Controller_pagofacebook extends CI_Controller {
           if (!isset ($permissions['data'][0]['publish_stream']))
             throw new Exception('No tengo permiso publish_stream', $NO_PUBLISH_STREAM);
 
-          $mensaje='Probando la publicación de mensajes en Facebook...';
+          $mensaje='He comprado software con pago social en Massive Dynamic, un universo en software, reviza sus ofertas en http://massivedynamic.inf.utfsm.cl/';
           $facebook->api('/me/feed', 'post', array ('message' => $mensaje));
 
         } catch (Exception $e)
