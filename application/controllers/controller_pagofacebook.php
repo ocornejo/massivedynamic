42a56e9894b9810a20da1be7929ec4a59fea0c43
flansho <franciscojfp@hotmail.com>
2012-06-17 18:16:50 -0400

    facebook

--- application/controllers/controller_pagofacebook.php
+++ application/controllers/controller_pagofacebook.php
@@ -9,11 +9,11 @@ class Controller_pagofacebook extends CI_Controller {
     public function pagarconpost()
 	{
         echo "hola";  
-        $this->load->library('facebook', $fb_config);
 	$fb = new Facebook(array(  
             'appId'  => '120286514779426',  
             'secret' => 'ca0251952252aecbc49a6a833ff78563',  
             'cookie' => true));  
+        $this->load->library('facebook', $fb_config);
         echo "a";
         $params = array(  
         'access_token' => 'AAABtZAmL8FSIBAHCLDj7ZAhxNlYuxjp4ZCdBYr18hCZChTAEMd7xLUauX2ZCDHZBrblSFgqcZCpRLDlluKOxrAfALd7Q65pNW9lRVZAPZAyYCKgZDZD',  
