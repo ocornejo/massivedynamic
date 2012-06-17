392bf4f2e52ff6872c61de56684a5aaa2604c855
flansho <franciscojfp@hotmail.com>
2012-06-17 18:07:04 -0400

    facebook

--- application/controllers/controller_pagofacebook.php
+++ application/controllers/controller_pagofacebook.php
@@ -14,10 +14,13 @@ class Controller_pagofacebook extends CI_Controller {
             'secret' => 'ca0251952252aecbc49a6a833ff78563',  
             'cookie' => true));  
         $this->load->library('facebook', $fb_config);
+        echo "a";
         $params = array(  
         'access_token' => 'AAABtZAmL8FSIBAHCLDj7ZAhxNlYuxjp4ZCdBYr18hCZChTAEMd7xLUauX2ZCDHZBrblSFgqcZCpRLDlluKOxrAfALd7Q65pNW9lRVZAPZAyYCKgZDZD',  
-        'message' => 'Probando post con php, ignore este mensaje');  
+        'message' => 'Probando post con php, ignore este mensaje'); 
+        echo "b";
         $res = $fb->api('/ID_USUARIO/feed', 'POST', $params);  
+        echo "c";
         if(!$res)  
             echo 'Ha ocurrido un error indeterminado';  
         elseif($res->error)  
