b55dde0e45df667d0e1ba8700c5f65b27635d28b
flansho <franciscojfp@hotmail.com>
2012-06-20 01:03:07 -0400

    producto

--- application/views/ficha_producto.php
+++ application/views/ficha_producto.php
@@ -125,7 +125,7 @@
                             <td valign="right" width="200"><center><b>
                                 <?php 
                                 foreach ($producto->result() as $comprado){
-                                if($comprado!=null){
+                                if($comprado==null){
                                     echo "Agregar al carrito:";}
                                     else{
                                         echo "Usted ya compró este producto";
