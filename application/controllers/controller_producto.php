5bee35ad4cdbfe38e97c34f893f935c842e61e49
flansho <franciscojfp@hotmail.com>
2012-06-20 01:31:19 -0400

    producto

--- application/controllers/controller_producto.php
+++ application/controllers/controller_producto.php
@@ -7,6 +7,7 @@ class Controller_producto extends CI_Controller{
         $this->load->model('model_catalogo');
         $this->load->model('model_producto');
         $this->load->library('session');
+        $data['compra'] =false;
         $comprado=$this->model_producto->get_compra($this->session->userdata('idUsuario'),$id);
         if($comprado){$data['compra'] =true;}
         else{$data['compra'] = false;}
