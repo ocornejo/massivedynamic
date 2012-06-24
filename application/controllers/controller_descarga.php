f246ce91d9563e6b95a5c2de96185b9e77d18ebb
flansho <franciscojfp@hotmail.com>
2012-06-24 11:17:43 -0400

    descarga

--- application/controllers/controller_descarga.php
+++ application/controllers/controller_descarga.php
@@ -3,7 +3,7 @@ class Controller_descarga extends CI_Controller {
     
     function bajar($codigo)
     {
-    $ruta="/programas/".$codigo.".txt";
+    $ruta=base_url()."/programas/".$codigo.".txt";
     $Datos = file_get_contents($ruta); //Leer el contenido del archivo 
     $nombre = 'MyPhoto.jpg'; 
     force_download($nombre,$Datos); 
