a3924d59a4a2bdd4dbdccd353ff530d5750b62d7
flansho <franciscojfp@hotmail.com>
2012-06-24 19:28:12 -0400

    grabar compra en bd

--- application/controllers/controller_pagofacebook.php
+++ application/controllers/controller_pagofacebook.php
@@ -58,6 +58,7 @@ class Controller_pagofacebook extends CI_Controller {
           $this->load->library('session');
           while(isset ($_POST['codigo'.$num])){
           $this->model_compra->IngresarCompra($this->session->userdata('idUsuarios'),$_POST['codigo'.$num],1);
+          echo "Su usuario es ".$this->session->userdata('idUsuarios')." y ha comprado el producto con codigo ".$_POST['codigo'.$num];
           }
         } catch (Exception $e)
         {
