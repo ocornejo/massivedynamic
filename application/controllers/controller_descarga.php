992f472bd71181a01677f471cf6cf4e25f1457cc
flansho <franciscojfp@hotmail.com>
2012-06-24 11:27:10 -0400

    descarga

--- application/controllers/controller_descarga.php
+++ application/controllers/controller_descarga.php
@@ -4,10 +4,11 @@ class Controller_descarga extends CI_Controller {
     function bajar($codigo)
     {
     $ruta=base_url()."/programas/".$codigo.".txt";
-    $Datos = file_get_contents($ruta); //Leer el contenido del archivo 
-    $nombre = 'MyPhoto.jpg'; 
-    force_download($nombre,$Datos); 
-    $this->load->view('view_catalogo',$data); 
+    $datos = file_get_contents($ruta); //Leer el contenido del archivo 
+    $nombre = $codigo.'txt'; 
+    force_download($nombre,$datos);
+    $data[mensaje]=$ruta;
+    $this->load->view('view_comprado',$data); 
     }
 }
 ?>
