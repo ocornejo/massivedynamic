5c6f9dc68110ef1cb15ab989faffb2d79fd2cc72
flansho <franciscojfp@hotmail.com>
2012-06-24 19:11:18 -0400

    grabar compra en bd

--- application/models/model_compra.php
+++ application/models/model_compra.php
@@ -8,7 +8,7 @@ class Model_registro extends CI_Model{
         $DB1=$this->load->database('default',TRUE);
         //se setea el producto comprado, el usuario que lo compra, como lo paga (0 para paypal y 1 para facebook) y la fecha
         $data = array('idProducto' => $producto,'idUsuaios' => $usuario,'idMedioPago' => $pago, 'Fecha'=>date("Y-m-d"));
-        $str = $DB1->insert_string('Usuarios', $data);
+        $str = $DB1->insert_string('Compra', $data);
         $DB1->query($str);
         return ;
         
