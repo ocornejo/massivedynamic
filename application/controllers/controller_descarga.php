14288ff1e155e617d74f115723981605c3982064
flansho <franciscojfp@hotmail.com>
2012-06-24 22:24:50 -0400

    descarga

--- application/controllers/controller_descarga.php
+++ application/controllers/controller_descarga.php
@@ -6,9 +6,8 @@ class Controller_descarga extends CI_Controller {
     $this->load->helper('download');
     $ruta=base_url()."programas/".$codigo.".md";
     $datos = file_get_contents($ruta); //Leer el contenido del archivo 
-    $nombre = $codigo.'md'; 
+    $nombre = $codigo.'md';
     force_download($nombre, $datos);
-    echo $ruta;
     }
 }
 ?>
