03c7f49a0d4cb3ad11ada7b10fec7b7985b81828
flansho <franciscojfp@hotmail.com>
2012-06-24 21:44:01 -0400

    arreglo login a catalogo

--- application/views/view_catalogo.php
+++ application/views/view_catalogo.php
@@ -111,6 +111,7 @@
     <div id="container">
         <section id="intro">
             <center>
+                <?php echo $this->pagination->create_links(); ?>
             <?php
                 foreach($resultado->result() as $row){ ?>
                 <table border="0">
