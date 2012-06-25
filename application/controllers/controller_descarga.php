8171f76d1430f1f0f5abd8b4b9479358f2f7c15e
flansho <franciscojfp@hotmail.com>
2012-06-24 21:58:23 -0400

    descarga

--- application/controllers/controller_descarga.php
+++ application/controllers/controller_descarga.php
@@ -7,9 +7,10 @@ class Controller_descarga extends CI_Controller {
     $ruta=base_url()."programas/".$codigo.".txt";
     $datos = file_get_contents($ruta); //Leer el contenido del archivo 
     $nombre = $codigo.'txt'; 
-    force_download($nombre,$datos);
-    $data["mensaje"]=$ruta;
-    $this->load->view('view_comprado',$data); 
+    $file_data =  read_file($ruta);
+    force_download($nombre, $file_data);
+    echo $ruta;
+    
     }
 }
 ?>
