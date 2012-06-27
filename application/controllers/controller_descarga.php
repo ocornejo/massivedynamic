ab6bcfd6f60539c37431c33c2cb3f2c916e873b2
flansho <franciscojfp@hotmail.com>
2012-06-27 11:21:56 -0400

    facebook

--- application/controllers/controller_descarga.php
+++ application/controllers/controller_descarga.php
@@ -29,7 +29,16 @@ class Controller_descarga extends CI_Controller {
     $mime = get_mime_by_extension($path);
 
     // Build the headers to push out the file properly.
-    
+    header('Pragma: public');     // required
+    header('Expires: 0');         // no cache
+    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+   // header('Last-Modified: '.gmdate ('D, d M Y H:i:s', filemtime ($path)).' GMT');
+    header('Cache-Control: private',false);
+    header('Content-Type: '.$mime);  // Add the mime type from Code igniter.
+    header('Content-Disposition: attachment; filename="'.basename($name).'"');  // Add the file name
+    header('Content-Transfer-Encoding: binary');
+    header('Content-Length: '.filesize($path)); // provide file size
+    header('Connection: close');
     readfile($path); // push it out
     exit();
     
