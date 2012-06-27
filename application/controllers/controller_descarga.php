3d9fe92bfe780124024fc16a83b089a264138575
flansho <franciscojfp@hotmail.com>
2012-06-27 10:43:37 -0400

    facebook

--- application/controllers/controller_descarga.php
+++ application/controllers/controller_descarga.php
@@ -8,7 +8,9 @@ class Controller_descarga extends CI_Controller {
     echo $ruta;
     $datos = file_get_contents($ruta); //Leer el contenido del archivo 
     $nombre = $codigo.'md';
+    echo "hola";
     $logrado=force_download($nombre, $datos);
+    echo "chao";
     echo $logrado;
     }
 }
