1216e248b433961d21743b14259f6feb5c6fefc2
flansho <franciscojfp@hotmail.com>
2012-06-27 11:17:49 -0400

    facebook

--- application/controllers/controller_descarga.php
+++ application/controllers/controller_descarga.php
@@ -4,7 +4,7 @@ class Controller_descarga extends CI_Controller {
     function bajar($codigo)
     {
     $this->load->helper('download');
-    $path=base_url()."programas/".$codigo.".rar";
+    $path=base_url()."programas/".$codigo.".md";
     echo $path;
     //$datos = file_get_contents($ruta); //Leer el contenido del archivo 
     $name = $codigo.'md';
