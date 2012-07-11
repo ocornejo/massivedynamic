327a2579393e28bf7b155870e4b95bdf6514a178
flansho <franciscojfp@hotmail.com>
2012-07-11 15:18:11 -0400

    logout

--- application/libraries/Session2.php
+++ application/libraries/Session2.php
@@ -10,7 +10,7 @@
 * @author      Dariusz Debowczyk
 * @link        http://www.codeigniter.com/user_guide/libraries/sessions.html
 */
-class CI_Session {
+class CI_Session2 {
 
 	var $flash_key = 'flash'; // prefix for "flash" variables (eg. flash:new:message)
 
